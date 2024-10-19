fe4743d73239f74cde39285fc3e2d6671b90ea61
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-10-19 16:03:31 +0700

    fix: bug import

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./button.module.scss";
+import styles from "./Button.module.scss";
 
 type Proptypes = {
   type: "button" | "submit" | "reset" | undefined;
--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./input.module.scss";
+import styles from "./Input.module.scss";
 
 type Proptypes = {
   label?: string;
